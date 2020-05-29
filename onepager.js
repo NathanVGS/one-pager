a588f9f4d9be2302cd7241533b3eda479584f762
Nathan Van Gulck <vangulcknathan@gmail.com>
2020-05-29 14:40:45 +0200

    update

--- onepager.js
+++ onepager.js
@@ -1,5 +1,5 @@
 let visitors_counter = document.getElementById('Visitors').innerText;
-let volunteers_counter = document.getElementById('Volunteers').innerText;
+//let volunteers_counter = document.getElementById('Volunteers').innerText;
 
 setTimeout(function() {
     odometer.innerText = parseInt(visitors_counter);
