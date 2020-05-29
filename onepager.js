3f5eb0e7ba3da615650314b7842abe8f9727ca04
Tim-Smet <tim2smet@gmail.com>
2020-05-29 14:24:02 +0200

    odometer works

--- onepager.js
+++ onepager.js
@@ -1,6 +1,11 @@
 let visitors_counter = document.getElementById('Visitors').innerText;
-//let volunteers_counter = document.getElementById('Volunteers').innerText;
+let volunteers_counter = document.getElementById('Volunteers').innerText;
 
 setTimeout(function() {
     odometer.innerText = parseInt(visitors_counter);
-}, 200);
+  }, 200);
+
+let test = document.getElementsByClassName("carousel-control-next");
+test[0].addEventListener("click", function(){
+    clearInterval(myVar);
+});
